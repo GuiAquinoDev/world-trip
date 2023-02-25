c5ad54b3b348f86bfe311890e7b9f1fac3c4483e
Guilherme Aquino <guiiaquino92@gmail.com>
2023-02-25 16:21:58 -0300

    feat: Prismic config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '../../prismicio'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </PrismicPreview>
+    </PrismicProvider>
+  )
 }
