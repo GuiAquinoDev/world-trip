5c9b6656ef9e6a586032c160afbdb63fd6a88c5d
Guilherme Aquino <guiiaquino92@gmail.com>
2023-03-11 17:12:59 -0300

    fix: error slice config

--- .slicemachine/assets/src/slices/Cities/index.stories.js
+++ .slicemachine/assets/src/slices/Cities/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"cityImage":{"dimensions":{"width":256,"height":173},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1576662712957-9c79ae1280f8?rect=0%2C560%2C3456%2C2336&w=256&h=173"},"cityName":"secret","stateName":"rain","stateFlag":{"dimensions":{"width":4752,"height":3168},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1589652717521-10c0d092dea9"}}],"primary":{"title":"at"},"id":"_Default","slice_type":"cities"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"cityImage":{"dimensions":{"width":256,"height":173},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1600804931749-2da4ce26c869?rect=0%2C519%2C3199%2C2162&w=256&h=173"},"cityName":"beautiful","stateName":"stop","stateFlag":{"dimensions":{"width":4752,"height":3168},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1589652717521-10c0d092dea9"}}],"primary":{"title":"grass"},"id":"_Default","slice_type":"cities"}} />
 _Default.storyName = ''
